c439ba74d52c9813b363c3ac1885fac4999bb7f5
girardin-tarby-matheo <matheo.girardin--tarby@edu.univ-fcomte.fr>
2026-01-01 17:41:23 +0100

    pages autres projets

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// Menu //
+
 const toggle = document.getElementById('menuButton') || document.querySelector('.menu-button');
 const closeBtn = document.getElementById('menuClose');
 const nav = document.getElementById('mainNav') || document.querySelector('.menu');
@@ -12,3 +14,34 @@ closeBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
     page.classList.toggle('no-scroll');
 });
+
+
+// Carrousel //
+
+const carousel = document.querySelector("div.carousel__container");
+const prevButton = document.querySelector("button.carousel__button--prev");
+const nextButton = document.querySelector("button.carousel__button--next");
+
+if (carousel) {
+    const premierItem = document.querySelector(".carousel__item");
+
+    function getScrollAmount() {
+        return premierItem.clientWidth;
+    }
+
+    prevButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: -getScrollAmount(),
+            behavior: "smooth"
+        });
+    });
+
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: getScrollAmount(),
+            behavior: "smooth"
+        });
+    });
+    window.addEventListener("resize", () => {
+    });
+}
